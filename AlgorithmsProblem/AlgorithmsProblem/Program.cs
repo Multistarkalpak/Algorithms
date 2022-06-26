c62d30d523d0a36902f5da52fa7af5e79ee41dce
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-26 11:36:49 +0530

    [kalpak] palidrome

--- AlgorithmsProblem/AlgorithmsProblem/Program.cs
+++ AlgorithmsProblem/AlgorithmsProblem/Program.cs
@@ -24,10 +24,34 @@
             //// Calling the function to print result
             //InsertionSort.printArraystring(arr1, n);
             //uc4:BubbleSort
-            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
-            BubbleSort.bubbleSort(arr);
-            Console.WriteLine("Sorted array");
-            BubbleSort.printArray(arr);
+            //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            //BubbleSort.bubbleSort(arr);
+            //Console.WriteLine("Sorted array");
+            //BubbleSort.printArray(arr);
+            ////uc5:MergeSort
+            //int[] arr = { 12, 11, 13, 5, 6, 7 };
+            //Console.WriteLine("Given Array");
+            //MergeSort.printArray(arr);
+            //MergeSort ob = new MergeSort();
+            //ob.sort(arr, 0, arr.Length - 1);
+            //Console.WriteLine("\nSorted array");
+            //MergeSort.printArray(arr);
+            ////uc6
+            string str1 = "heart";
+            string str2 = "earth";
+
+            // Function call
+            if (Anagram.areAnagram(str1, str2))
+                Console.Write("The two strings are " +
+                              "anagram of each other");
+            else
+                Console.Write("The two strings are " +
+                              "not anagram of each other");
+            //uc7
+            //PrimeRange.PrimeNumber();
+            ////uc8.1
+
+            //PrimePalindrome.countPal(100, 1000);
         }
     }
 }
